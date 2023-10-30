725fee5b3591b29fc2e653ec00b78b68f7f8e239
Prince <prince.affum45@gmail.com>
2023-10-30 08:22:05 -0700

    task 11

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,26 @@
 #include "lists.h"
-
-/**
+#include <stdlib.h>
+/** 
  * check_cycle - it checks if a singly linked list contain a cycle
  * @list: a singly linked list
- * return: 0 if their is no cycle and 1 if their is a cycle
+ * return: o if their is no cycle and 1 if their is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *five;
-	listint_t *six;
+	listint_t *three, *four;
 
-	while (list != NULL && list->next != NULL)
+	if(list == NULL|| list->next == NULL)
+		return (0);
+	three = list->next;
+	four = list->next->next;
+
+	while (three && four && four->next)
 	{
-		five = list->next;
-		six = list->next->next;
-
-		if (five == six)
-		{
-			return (1);
-		}
+		if (three == four)
+			return(1);
+		three = three->next;
+		four = four->next->next;
 	}
 	return (0);
+
 }
