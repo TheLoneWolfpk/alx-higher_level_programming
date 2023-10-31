cdfacf7021d0d05e12f77fa25b3454aa7263e1f7
Prince <prince.affum45@gmail.com>
2023-10-31 08:28:45 -0700

    task 9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def make_upper(letter):
-    if 96 < ord(letter) < 123:
-        return(chr(ord(letter) - 32))
-    else:
-        return(chr(ord(letter)))
-
-
 def uppercase(str):
-    upper_string = ""
-    for letter in str:
-        upper_string += "%c" % make_upper(letter)
-    print("{}".format(upper_string))
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
